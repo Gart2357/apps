2739f9c115595f2e149eb0ebc9726af6db066658
Gart2357 <gart@outlook.jp>
2020-04-09 01:01:53 +0900

    arithmetic derivative

---
+++ js/ad.js
@@ -0,0 +1,46 @@
+function ad()
+{
+    var n = parseInt(ad_form.textbox.value);
+    if (!n || n < 1){
+        alert('正整数のみ対応しています');
+        return;
+    }
+    
+    var is_prime = new Array(n+1).fill(1);
+    is_prime[0] = is_prime[1] = 0;
+    for (var i = 2; i <= Math.sqrt(n); i++){
+        if (is_prime[i]){
+            for (var j = i * 2; j <= n; j += i){
+                is_prime[j] = 0;
+            }
+        }
+    }
+    
+    var nn = n;
+    var fct = {};
+    for (var i = 2; i <= n; i++){
+        if (!is_prime(i) || nn % i){
+            continue;
+        }
+
+        fct[i] = 0;
+        while (nn % i){
+            nn /= i;
+            fct[i]++;
+        }
+    }
+
+    var fcts = n + '=';
+    var d = 0;
+    Object.keys(fct).forEach(function(base){
+        var exp = this[base];
+        d += n / base * exp;
+        if (exp == 1){
+            fcts += base + ' × ';
+        }else{
+            fcts += base + '^' + exp + ' × '
+        }
+    }, fct);
+
+    alert(fcts.slice(0, -2) + '\n' + n + '\'=' + d);
+}
